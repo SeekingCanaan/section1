c343c24a4744b360f8edde118de19cb8f2f77599
SeekingCanaan <348659570@qq.com>
2021-07-06 17:18:04 +0800

    第五次提交

---
+++ src/Solution609.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+// 609.在系统中查找重复文件
+public class Solution609 {
+
+    public static void main(String[] args) {
+        System.out.println(findDuplicate(new String[]{"root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"}));
+    }
+
+    public static List<List<String>> findDuplicate(String[] paths) {
+
+        // 定义map，保存结果
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String path : paths) {
+
+            String[] values = path.split(" ");
+            System.out.println(Arrays.toString(values));
+            System.out.println("-----------------------");
+            for (int i = 1; i < values.length; i++) {
+
+                // 将文件名和文件内容分开，中间有一个 ( , 那么内容保存在 name_cont[1]中,后面还带了个 )
+                String[] name_cont = values[i].split("\\(");
+                // 去掉内容后面的 )
+                name_cont[1] = name_cont[1].replace(")", "");
+                // 将
+                List<String > list = map.getOrDefault(name_cont[1], new ArrayList<>());
+
+                // values[0] + "/" + name_cont[0] 将文件路径合并，并添加到list集合中
+                list.add(values[0] + "/" + name_cont[0]);
+
+                System.out.println(name_cont[1]);
+                System.out.println(list.toString());
+                System.out.println("==========");
+                map.put(name_cont[1], list);
+            }
+
+        }
+        return new ArrayList<>(new ArrayList<>());
+    }
+}
--- src/Solution64.java
+++ src/Solution64.java
@@ -1,7 +1,42 @@
 // 64.最小路径和
 public class Solution64 {
 
-    public int minPathSum(int[][] grid) {
-        return 1;
+    public static void main(String[] args) {
+        System.out.println(minPathSum(new int[][]{{1,3,1}, {1,5,1}, {4,2,1}}));
+    }
+
+    public static int minPathSum(int[][] grid) {
+
+        // 如果 gird 为 null 或者 grid长度为0，或者grid[0]的长度为0，即没有了起点，返回0
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+
+        // 定义rows为grid长度，即行数；cols为grid[0]的长度，即列数
+        int rows = grid.length, cols = grid[0].length;
+
+        // 定义二维数组dp保存结果
+        int[][] dp = new int[rows][cols];
+        // 将dp[0][0]设置为grid[0][0]，即刚开始要计算原点上的数字
+        dp[0][0] = grid[0][0];
+
+        // 将dp的第一列计算出来，由该位置上方的值 + grid在该点的值
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+
+        // 将dp的第一行计算出来，由该位置左方的值 + grid在该点的值
+        for (int i = 1; i < cols; i++) {
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+
+        // 从dp[1][1]开始计算，dp该点的值为该点上方的值和左边的值中的最小值 + grid在该点的值
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
+            }
+        }
+
+        // 最后路径和最小就保存在dp[rows-1][cols-1]中
+        return dp[rows-1][cols-1];
     }
 }
